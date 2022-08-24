d55eb5813b60ce1918f88689ff09dc6ab62fc42b
Daniel Ballesteros Li <felipe.ballesteros11@gmail.com>
2022-08-23 20:28:44 -0500

    Aplicacion de consola .net desde linux

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+System.Console.WriteLine("escriba un Nombre");
+int algo;
+string some = Console.ReadLine();
+
+bool somee = int.TryParse(some, out algo);
+
+List<string> li = new List<string>();
+
+li.Add(some);
+
+foreach(string item in li){
+    System.Console.WriteLine(item);
+}
+
+Console.WriteLine("Hello, World!");
